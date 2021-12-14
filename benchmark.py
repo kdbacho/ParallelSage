e90878a74ac7d96c3c65a8e84666d7a3eb6cc432
jdevaranjan <jdevaranjan@nvidia.com>
2021-12-14 14:15:57 -0500

    Pushed Sample Graphs

--- benchmark.py
+++ benchmark.py
@@ -14,6 +14,8 @@ parser.add_argument('--B', type=int, default=300)
 parser.add_argument('--L', type=int, default=3)
 parser.add_argument('--k', type=int, default=5)
 parser.add_argument('--procs', type=int, default=8)
+parser.add_argument('--trials', type=int, default=1)
+parser.add_argument('--sb_acc_trials', type=int, default=0)
 args = parser.parse_args()
 
 
@@ -54,7 +56,7 @@ def parallel_sample_nodes(vs, l):
 def tree_size(h):
     return (K ** (h + 1) - K) // (K - 1)
 
-def tree_sample(b, B, l):
+def tree_sample(b, B, l, parallel=True):
     leaf_set = set()
     batch_set = set()
         
@@ -84,10 +86,14 @@ def tree_sample(b, B, l):
             expans.append(set(samp_neigh_g(u)))
         batch_set = batch_set.union(*expans)
         leaf_set = leaf_set.union(*expans)
-    node_sets = parallel_sample_nodes(leaf_set, l)
-    
+    non_leafs = batch_set - leaf_set
+    if parallel:
+        node_sets = parallel_sample_nodes(leaf_set, l)
+    else:
+        node_sets = seq_sample_nodes(leaf_set, l)
+
     for i in range(l):
-        node_sets[i] = node_sets[i].union(batch_set)
+        node_sets[i] = node_sets[i].union(non_leafs)
     
     return  node_sets, batch_set, leaf_set
 
@@ -104,36 +110,89 @@ if __name__ == '__main__':
     L = args.L
     B = args.B
     K = args.k
+    T = args.trials
+
+    seq_s = 0
+    par_s = 0
+    seq_tree = 0
+    par_tree = 0
 
     # Sequential Standard
     print("Sequential Standard:")
-    t1 = time.time()
-    vs = sample(list(range(n)), B)
-    ls = seq_sample_nodes(vs, L)
-    t2 = time.time()
-    print(t2 - t1)
+    tot_time = 0
+    for i in range(T):
+        t1 = time.time()
+        vs = sample(list(range(n)), B)
+        ls = seq_sample_nodes(vs, L)
+        t2 = time.time()
+        tot_time += (t2 - t1)
+    seq_s = tot_time / T
+    print(tot_time, tot_time / T)
     for i in range(L):
         print("B(" + str(i) + ") size:", len(ls[i]))
     print()
 
     # Parallel Standard
     print("Parallel Standard:")
-    t1 = time.time()
-    ls = parallel_sample_nodes(vs, L)
-    t2 = time.time()
-    print(t2 - t1)
+    tot_time = 0 
+    for i in range(T):
+        t1 = time.time()
+        ls = parallel_sample_nodes(vs, L)
+        t2 = time.time()
+        tot_time += (t2 - t1)
+    par_s = tot_time / T
+    print(tot_time, tot_time /T)
     for i in range(L):
         print("B(" + str(i) + ")size:", len(ls[i]))
     print()
 
     # Tree
-    print("Tree:")
-    t1 = time.time()
-    ls, bs, leafs = tree_sample(vs[0], B, L)
-    t2 = time.time()
-    print(t2 - t1)
+    print("Tree Sequential:")
+    tot_time = 0
+    for i in range(T):
+        t1 = time.time()
+        v = sample(list(range(n)),1)[0]
+        ls, bs, leafs = tree_sample(v, B, L, parallel=False)
+        t2 = time.time()
+        tot_time += (t2 - t1)
+    seq_tree = tot_time / T
+    print(tot_time, tot_time / T)
     for i in range(L):
         print("B(" + str(i) + ") size:", len(ls[i]))
     print("effective_batch_size:", len(bs))
     print("computation_batch_size:", len(leafs))
+
+    # Tree
+    print("Tree Parallel:")
+    tot_time = 0
+    for i in range(T):
+        t1 = time.time()
+        v = sample(list(range(n)),1)[0]
+        ls, bs, leafs = tree_sample(v, B, L, parallel=True)
+        t2 = time.time()
+        tot_time += (t2 - t1)
+    par_tree = tot_time / T
+    print(tot_time, tot_time / T)
+    for i in range(L):
+        print("B(" + str(i) + ") size:", len(ls[i]))
+    print("effective_batch_size:", len(bs))
+    print("computation_batch_size:", len(leafs))
+
+    print("Speedups", (seq_s / par_s), (seq_s / seq_tree), (seq_s / par_tree))
+
+    if args.sb_acc_trials != 0:
+        print("Testing Sb accuracy and shrinkage")
+        num_t = args.sb_acc_trials 
+        tot_rel_error = 0
+        tot_shrink = 0
+        for i in range(args.sb_acc_trials):
+            v= sample(list(range(n)), 1)[0]
+            # Depth of network doesn't matter here
+            ls, bs, leafs = tree_sample(v,B,1,parallel=True)
+            tot_rel_error += abs(B - len(bs)) /len(bs)
+            tot_shrink += B / len(leafs)
+        print("avg rel error", tot_rel_error / num_t)
+        print("avg shrink", tot_shrink / num_t)
+
+
 
